3622a963a211887c8da81e74e7d6342a8a97822b
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2025-11-04 21:35:58 +0800

    Implement user service layer with getUserByEmail method

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,1 +1,12 @@
 // User business logic
+import { db } from "@/shared/config/database.js";
+import { user } from "./user.model.js";
+import { eq } from 'drizzle-orm';
+
+export class UserService {
+
+    async getUserByEmail(email: string) {
+        const myUser = await db.select().from(user).where(eq(user.email, email)).limit(1);
+        return myUser[0];
+    }
+}
