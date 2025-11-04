ea7bf0067529c627b317fe3595fa92902c7f9654
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2025-11-04 20:23:18 +0800

    refactor: use environment variable for BETTER_AUTH_URL

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -8,14 +8,12 @@ export const auth = betterAuth({
         provider: "pg",
         schema: schema,
     }),
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:8000",
+    baseURL: process.env.BETTER_AUTH_URL, //http://localhost:8000 if developing locally
     socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }, 
     },
-    trustedOrigins: [
-        "http://localhost:3000",
-      ]
+    trustedOrigins: process.env.TRUSTED_ORIGINS?.split(",") || ["http://localhost:3000"],
 });
