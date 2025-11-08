5cd94084e883cf8f8accf4a34e73bc4972f38c81
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2025-11-08 17:35:15 +0800

    chore: removed allowed headers in middleware

--- src/shared/middleware/auth.middleware.ts
+++ src/shared/middleware/auth.middleware.ts
@@ -5,5 +5,4 @@ export const corsMiddleware = cors({
   origin: process.env.CLIENT_URL, // Next.js client origin
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Specify allowed HTTP methods
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
 });
