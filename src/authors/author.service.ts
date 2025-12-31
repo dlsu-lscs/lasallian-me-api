cec13a0bb418f68b0e0f8267574c9b4f3e3890ac
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2025-12-31 11:22:45 +0800

    fix: Use arrow functions for "this" binding

--- src/authors/author.service.ts
+++ src/authors/author.service.ts
@@ -1,28 +1,27 @@
 import {db} from "@/shared/config/database.js"
 import {author} from "@/shared/infrastructure/database/schema.js"
 import {eq} from "drizzle-orm"
-
-export type SelectAuthor = typeof author.$inferSelect
-export type InsertAuthor = typeof author.$inferInsert
+import type { SelectAuthor, InsertAuthor } from "./author.model.js"
 
 export default class AuthorService{
 
-    async getAuthor(email: string)
-    : Promise<SelectAuthor | undefined>
+    getAuthor = async(email: string)
+    : Promise<SelectAuthor | undefined> =>
     {
         const [result] = await db.select().from(author).where(eq(author.email, email)).limit(1)
         return result
     }
 
-    async createAuthor(newAuthor: InsertAuthor)
-    :Promise<SelectAuthor>
+    createAuthor = async(newAuthor: InsertAuthor)
+    :Promise<SelectAuthor> =>
     {
         const [myAuthor] = await db.insert(author).values(newAuthor).returning()
         return myAuthor
     }
 
-    async deleteAuthor(authorId: number)
-    :Promise<SelectAuthor>{
+    deleteAuthor = async(authorId: number)
+    :Promise<SelectAuthor> => 
+    {
         const [myAuthor] = await db.delete(author).where(eq(author.id, authorId)).returning()
         return myAuthor
     }
