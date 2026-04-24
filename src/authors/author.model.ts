afbbf5432418cf402bce349dfb0d47534f7b16eb
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2026-04-24 20:48:40 +0800

    refactor: deleted author to merge with user

--- src/authors/author.model.ts
+++
@@ -1,7 +0,0 @@
-import { author } from "@/shared/infrastructure/database/schema.js"
-export { author } from "@/shared/infrastructure/database/schema.js";
-
-
-
-export type SelectAuthor = typeof author.$inferSelect
-export type InsertAuthor = typeof author.$inferInsert
--- src/authors/dto/index.ts
+++
@@ -1,4 +0,0 @@
-export { AuthorRequestSchema, type AuthorRequest } from "./author-request.dto.js";
-export { CreateAuthorRequestSchema, type CreateAuthorRequest } from "./create-author-request.dto.js";
-export { AuthorResponseSchema, AuthorsListResponseSchema, type AuthorResponse, type AuthorsListResponse } from "./author-response.dto.js";
-export { DeleteAuthorRequestSchema, type DeleteAuthorRequest} from "./delete-author-request.dto.js"
