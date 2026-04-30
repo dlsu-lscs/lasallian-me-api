ef067e8592bba3ea4db8f9cedd281522203ae448
Jean Luc <149998953+Jeyshel@users.noreply.github.com>
2026-05-01 02:21:04 +0800

    fix: removed an import of a non-existing file due to refactoring in dto index

--- src/ratings/dto/index.ts
+++ src/ratings/dto/index.ts
@@ -2,4 +2,3 @@ export * from './application-rating-request.dto.js';
 export * from './create-rating-request.dto.js';
 export * from './patch-rating-request.dto.js';
 export * from './ratings-response.dto.js';
-export * from './user-ratings-request.dto.js';
